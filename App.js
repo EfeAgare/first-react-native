cd7ce78751effe6bee877a68025c607f5892cdd1
Efe <knowledgeagare157@gmail.com>
2019-09-21 09:08:37 +0100

    add styles and css

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   const [outputText, setOutputText] = useState(
     'Open up App.js to start working on your app!'
   );
   return (
-    <View style={styles.container}>
-      <Text></Text>
-      <Button title="" />
+    // <View style={styles.container}>
+    //   <Text>{outputText}</Text>
+    //   <Button title="Change Text" onPress={() => setOutputText("This is my first App changing")} />
+    // </View>
+    <View style={{padding: 30}}>
+      <View>
+        <TextInput placeholder="Course Goal" style={{borderBottomColor: "black", borderWidth:1, padding: 10, marginTop: 10, marginBottom: 10}}/>
+        <Button title="ADD"/>
+      </View>
+      <View></View>
     </View>
   );
 }
